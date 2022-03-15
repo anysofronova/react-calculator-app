4762648d91284c53ee55c8d75a2363aec6be6cef
Anna <anysofronova@yandex.ru>
2022-03-15 18:40:11 +0300

    The structure is created

--- react-calculator-app/src/App.jsx
+++ react-calculator-app/src/App.jsx
@@ -1,7 +1,34 @@
 import "./style.scss";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="calculator__grid">
+      <div className="calculator__container">
+        <div className="calculator__output">
+          <div className="calculator__previous"></div>
+          <div className="calculator__current"></div>
+        </div>
+        <button className="calculator__span-two">AC</button>
+        <button>DEL</button>
+        <button>÷</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>*</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>-</button>
+        <button>.</button>
+        <button>0</button>
+        <button className="calculator__span-two">=</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
